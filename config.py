846a2431ade70acee5ea109b1673331a6f20cbb3
Parth Rakholiya <70030573+parthrakholiya@users.noreply.github.com>
2023-03-07 10:12:32 +0530

    saved updated files

---
+++ config.py
@@ -0,0 +1,6 @@
+customhost = "employee.cldufvfp8l2d.eu-central-1.rds.amazonaws.com"
+customuser = "admin"
+custompass = "Parth972000"
+customdb = "employee"
+custombucket = "employeead"
+customregion = "eu-central-1"
